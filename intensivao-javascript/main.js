e99cb92020506dbb9f7d175a3510c55c49c482d5
m3nininho <luiz.menino22@gmail.com>
2024-04-20 13:38:50 -0300

    adicionado o intensivao de javascript

---
+++ intensivao-javascript/main.js
@@ -0,0 +1,136 @@
+// 1. Crie um script que exiba a mensagem "Hello World!" em um alerta no navegador.
+
+// alert('Hello World')
+
+// ----------------------------------------
+
+// 2. Crie um script que declare duas variáveis e exiba o resultado da soma entre elas.
+
+// function declaration(){
+//     const name = 'Luiz'
+//     const age = 22
+
+//     return alert(`Olá meu nome é ${name} e tenho ${age} anos atualmente.`)
+// }
+// declaration()
+
+// ----------------------------------------
+
+// 3. Crie um script que declare uma variável e verifique se o seu valor é um número. Se for, exiba a mensagem "É um número", caso contrário, exiba a mensagem "Não é um número".
+
+// function verification(){
+//     const unknown = 22
+
+//      if(typeof unknown == "number"){
+//         alert('Isso é um Número');
+//         return
+//     } else{
+//         alert('Isso não é um número')
+//     }
+// }
+
+// verification()
+
+// ----------------------------------------
+
+// 4. Crie um script que declare uma variável e verifique se o seu valor é uma string. Se for, exiba a mensagem "É uma string", caso contrário, exiba a mensagem "Não é uma string".
+
+// function verification(){
+
+//     const unknown = 'Foi aqui'
+
+//     if(typeof unknown == "string"){
+//         alert('É uma string')
+//         return
+//     } else {
+//         alert('Não é uma string')
+//         return
+//     }
+// }
+// verification()
+
+// ----------------------------------------
+
+// 5. Crie um script que declare uma variável e verifique se o seu valor é um booleano. Se for, exiba a mensagem "É um booleano", caso contrário, exiba a mensagem "Não é um booleano".
+
+// function verification(){
+//     const unknown = true;
+
+//     if(typeof unknown == 'boolean'){
+//         alert('É um booleano')
+//     } else{
+//         alert('Não é um booleano')
+//     }
+// }
+
+// verification()
+
+// ----------------------------------------
+
+// 6. Crie um script que declare duas variáveis e exiba o resultado da subtração entre elas.
+
+// function declaration(){
+//     const one = 4;
+//     const two = 2;
+
+//     alert(one - two);
+// }
+
+// declaration();
+
+// ----------------------------------------
+
+// 7. Crie um script que declare duas variáveis e exiba o resultado da multiplicação entre elas.
+
+// function declaration(){
+//     const one = 10
+//     const two = 30;
+
+//     alert(one * two);
+// }
+
+// declaration()
+
+// ----------------------------------------
+
+// 8. Crie um script que declare duas variáveis e exiba o resultado da divisão entre elas.
+
+// function declaration() {
+//   const one = 20;
+//   const two = 2;
+
+//   alert(one / two);
+// }
+// declaration()
+
+// ----------------------------------------
+
+// 9. Crie um script que declare uma variável e verifique se o seu valor é um número par. Se for, exiba a mensagem "É um número par", caso contrário, exiba a mensagem "Não é um número par".
+
+// function declaration(){
+//     const unknown = 22
+
+//     if(unknown % 2 == 0){
+//         alert('é um número par')
+//     } else{
+//         alert('não é um número par')
+//     }
+// }
+
+// declaration()
+
+// ----------------------------------------
+
+// 10. Crie um script que declare uma variável e verifique se o seu valor é um número ímpar. Se for, exiba a mensagem "É um número ímpar", caso contrário, exiba a mensagem "Não é um número ímpar".
+
+// function declaration() {
+//   const unknown = 17;
+
+//   if (unknown % 2 == 1) {
+//     alert("Esse é um número impar");
+//   } else {
+//     alert("Não é um número impar");
+//   }
+// }
+
+// declaration();
